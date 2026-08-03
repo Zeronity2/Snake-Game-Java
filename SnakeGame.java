19368ef36e3b181a28c8f02e616dbc46a9c2bb2a
Khushi Patel <khushiwonder@gmail.com>
2026-08-03 22:51:32 +0530

    snake is growing after eating the food

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 public class SnakeGame extends PApplet{
   //intial windows setup
-  int snakeX = 300;
-  int snakeY = 200;
+  // int snakeX = 300;
+  // int snakeY = 200;
 
   int xSpeed = 0;
   int ySpeed = 0;
@@ -11,42 +13,61 @@ public class SnakeGame extends PApplet{
   int foodX = 100;
   int foodY=100;
 
+  ArrayList<Integer> snakeX = new ArrayList<>();
+  ArrayList<Integer> snakeY = new ArrayList<>();
+
   public void settings(){
     size(600,400);
   }
   
   public void setup(){
+    snakeX.add(300);
+    snakeY.add(200);
     background(30, 30, 40);
     noStroke();
     frameRate(8);
   }
 
-  public void draw(){
-    //container
-    fill(30, 30, 40);
-    rect(0, 0, width, height);
-    
-    snakeX += xSpeed;
-    snakeY += ySpeed;
+  public void draw() {
 
-    //snake movement
+    background(30,30,40);
+
+    // Move body
+    for(int i = snakeX.size() - 1; i > 0; i--){
+        snakeX.set(i, snakeX.get(i - 1));
+        snakeY.set(i, snakeY.get(i - 1));
+    }
+
+    // Move head
+    snakeX.set(0, snakeX.get(0) + xSpeed);
+    snakeY.set(0, snakeY.get(0) + ySpeed);
+
+    // Draw snake
     fill(255,0,0);
-    rect(snakeX, snakeY, 30,30);
-    //food
-    fill(0, 200, 255); 
-    ellipse(foodX, foodY, 20, 20);
 
-    //to check if snake eats food
-    if(snakeX < foodX + 20 &&
-      snakeX + 30 > foodX &&
-      snakeY < foodY + 20 &&
-      snakeY + 30 > foodY){
+    for(int i = 0; i < snakeX.size(); i++){
+        rect(snakeX.get(i), snakeY.get(i),30,30);
+    }
 
-        //move food to random location
-        foodX = (int) random(20, width-20);
-        foodY = (int) random(20, height - 20);
-      }
-  }
+    // Draw food
+    fill(0,200,255);
+    ellipse(foodX,foodY,20,20);
+
+    // Collision with food
+    if(snakeX.get(0) < foodX + 20 &&
+       snakeX.get(0) + 30 > foodX &&
+       snakeY.get(0) < foodY + 20 &&
+       snakeY.get(0) + 30 > foodY){
+
+        // Random food
+        foodX = (int)random(width - 20);
+        foodY = (int)random(height - 20);
+
+        // Grow snake
+        snakeX.add(snakeX.get(snakeX.size()-1));
+        snakeY.add(snakeY.get(snakeY.size()-1));
+    }
+}
   
   public static void main(String[] args){
       PApplet.main("SnakeGame");
@@ -55,20 +76,20 @@ public class SnakeGame extends PApplet{
   
   public void keyPressed(){
     if(keyCode == RIGHT){
-      xSpeed = 5;
+      xSpeed = 10;
       ySpeed = 0;
     }
     if(keyCode == LEFT){
-      xSpeed = -5;
+      xSpeed = -10;
       ySpeed = 0;
     }
     if(keyCode == UP){
       xSpeed = 0;
-      ySpeed = -5;
+      ySpeed = -10;
     }
     if(keyCode == DOWN){
       xSpeed = 0;
-      ySpeed = 5;
+      ySpeed = 10;
     }
   }
 } 
